22a9be3f9ef5b71d0be232aba62584d952dd491b
Kha Nguyen <94631848+dkhak3@users.noreply.github.com>
2026-09-10 17:44:49 +0700

    fix: xoa khung du trong BCCT HKVP

--- qlcldv/src/components/ManagedPageGate.jsx
+++
@@ -1,7 +0,0 @@
-import NotFoundPage from "../pages/NotFoundPage";
-import { usePageSettings } from "../PageSettingsContext";
-
-export default function ManagedPageGate({ pageKey, children }) {
-  const { getPage } = usePageSettings();
-  return getPage(pageKey)?.hidden ? <NotFoundPage/> : children;
-}
